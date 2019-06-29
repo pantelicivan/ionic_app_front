66a8ba30c54f1faa76ad388b2bb4844ffea64d7a
Milos Danilovic <milosdanilovic@Nikolas-MBP.localdomain>
2019-06-29 15:00:18 +0200

    Registration & Login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+@Injectable()
 export class LoginPage implements OnInit {
+  constructor(
+    private http: HttpClient,
+    private alert: AlertController,
+    private router: Router
+    ) { }
 
-  constructor() { }
+  email: string = ""
+  password: string = ""
 
   ngOnInit() {
   }
 
+  async login() {
+    const {email, password} = this
+    const url = "http://mobilno.develop/api/v1/login"
+
+    this.http.post(url, {
+      email: email,
+      password: password,
+    }).subscribe(response => {
+      this.showAlert("Success!", response['message'])
+    }, error => {
+      this.showAlert("Error!", error['error'].message)
+    });
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert =  await this.alert.create({
+      header,
+      message,
+      buttons: [{
+        text: "Ok",
+        handler: () => {
+          alert.dismiss();
+          if(header == "Success!") {
+            //Idemo dalje na main screen
+            this.router.navigate(['/login']);
+          }
+        }
+      }]
+    });
+
+    await alert.present()
+  }
 }
--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { AlertController } from '@ionic/angular'
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
+@Injectable()
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private alert: AlertController,
+    private router: Router
+    ) { }
 
+  name: string = ""
+  email: string = ""
+  password: string = ""
+  mobile_phone: string = ""
   ngOnInit() {
   }
 
+  register() {
+    const {name, email, password, mobile_phone} = this
+    
+    this.http.post("http://mobilno.develop/api/v1/register", {
+      name: name,
+      email: email,
+      password: password,
+      mobile_phone: mobile_phone
+    }).subscribe(response => {
+      this.showAlert("Success!", response['message'])
+    }, error => {
+      this.showAlert("Error!", error.message)
+    });
+  }
+
+  async showAlert(header: string, message: string) {
+    const alert =  await this.alert.create({
+      header,
+      message,
+      buttons: [{
+        text: "Ok",
+        handler: () => {
+          alert.dismiss();
+          if(header == "Success!") {
+            this.router.navigate(['/login']);
+          }
+        }
+      }]
+    });
+
+    await alert.present()
+  }
 }
